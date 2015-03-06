d3b11b015542dfddff856c271bef565752726188
ItsPriyesh <priyesh.96@hotmail.com>
2015-03-06 17:14:52 -0500

    Fix gitignore

--- app/src/main/java/io/prolabs/pro/SecretStuff.java
+++
@@ -1,8 +0,0 @@
-package io.prolabs.pro;
-
-public class SecretStuff {
-
-    public static final String LINKEDIN_API_KEY = "785s7f2efkecb6";
-    public static final String LINKEDIN_API_SECRET = "BNpwWM0BCSObpPVE";
-
-}
