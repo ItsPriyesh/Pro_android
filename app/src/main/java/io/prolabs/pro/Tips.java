d27fb0f9cf58c1d5638cbbbb867336123c2b2997
Edmund Noble <eanoble@uwaterloo.ca>
2015-03-08 15:48:42 -0400

    Added more tips.

---
+++ app/src/main/java/io/prolabs/pro/Tips.java
@@ -0,0 +1,14 @@
+package io.prolabs.pro;
+
+/**
+ * Created by Edmund on 2015-03-08.
+ */
+public class Tips {
+    public static final String[] TIPS = {
+            "Try to commit at least once every day!",
+            "Code something new every once and a while!",
+            "Try new languages!",
+            "Spread the word! The more forks and stars, the better!",
+            "Open your sources! We don't analyze private repositories!"
+    };
+}
