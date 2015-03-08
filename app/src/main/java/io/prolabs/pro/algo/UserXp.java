c7b0502037cbeed00e0ee7520cfef4e20c8cb077
Edmund Noble <eanoble@uwaterloo.ca>
2015-03-07 22:41:56 -0500

    Added stubs for XP calculation.

---
+++ app/src/main/java/io/prolabs/pro/algo/UserXp.java
@@ -0,0 +1,7 @@
+package io.prolabs.pro.algo;
+
+/**
+ * Created by Edmund on 2015-03-07.
+ */
+public class UserXp {
+}
---
+++ app/src/main/java/io/prolabs/pro/algo/XpCalculator.java
@@ -0,0 +1,8 @@
+package io.prolabs.pro.algo;
+
+/**
+ * Created by Edmund on 2015-03-07.
+ */
+public interface XpCalculator {
+    UserXp calculateXp(FullUserStats stats);
+}
