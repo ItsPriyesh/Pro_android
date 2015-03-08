a9c801aa858b3645518b4df38f6985658382a8f8
ItsPriyesh <priyesh.96@hotmail.com>
2015-03-07 21:04:35 -0500

    Release the secrets

---
+++ app/src/main/java/io/prolabs/pro/SecretStuff.java
@@ -0,0 +1,8 @@
+package io.prolabs.pro;
+
+public class SecretStuff {
+
+    public static final String LINKEDIN_API_KEY = "785s7f2efkecb6";
+    public static final String LINKEDIN_API_SECRET = "BNpwWM0BCSObpPVE";
+
+}
